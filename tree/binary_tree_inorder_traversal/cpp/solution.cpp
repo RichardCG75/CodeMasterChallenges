3fea6a1f7e43b063861f589db53f2dc498bbce9c
Ricardo Chacon <ricardo.chaconr6@gmail.com>
2024-08-08 20:56:10 -0600

    solution: inorder traversal binary tree solved

--- tree/binary_tree_inorder_traversal/cpp/solution.cpp
+++ tree/binary_tree_inorder_traversal/cpp/solution.cpp
@@ -1,10 +1,37 @@
 #include <vector>
+#include <stack>
 #include "solution.hpp"
 using std::vector;
+using std::stack;
 
 vector<int> Solution::inorderTraversal(TreeNode *root)
 {
-    /* CODE YOUR SOLUTION HERE */
-    return {}; // RETURN YOUR RESULT HERE
+    if (root == nullptr) return {};
+    vector<int> result;
+    stack<TreeNode *> stack;
+    TreeNode *current = root;
+
+    // Think of the stack as a way to remember where we are in the tree.
+    // Think of each node as a small tree.
+    // We traverse the leftmost node first, then the right node.
+
+    while (current != nullptr || !stack.empty())
+    {
+        // Find the leftmost node
+        while (current != nullptr)
+        {
+            stack.push(current);
+            current = current->left;
+        }
+
+        // At this point we have a nullptr, so we use the stack to get the last node
+        current = stack.top();
+        stack.pop();
+        result.push_back(current->val);
+
+        // Move to the right node
+        current = current->right;
+    }
+    return result;
 }
 
